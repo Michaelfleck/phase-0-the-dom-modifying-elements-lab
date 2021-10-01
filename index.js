514ee1d5a7e8468fe7b81813cca86e4dfb2b19c0
mfleck <michael.j.fleck@gmail.com>
2021-10-01 14:22:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here!
+
+
+
+main.remove(main);
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+newHeader.id = 'victory'
+victory.innerHTML = "<h1>Michael is the champion</h1>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
